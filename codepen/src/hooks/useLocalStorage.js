7a4af7480e280410664ec900610c7b972d18ba80
yjbae3974 <49051494+yjbae3974@users.noreply.github.com>
2021-10-15 18:13:42 +0900

    interpreter completed

---
+++ codepen/src/hooks/useLocalStorage.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {useState, useEffect} from 'react'
+const PREFIX = 'codepen-clone-'
+
+export default function useLocalStorage(key,initialValue) {
+    const prefixedkey = PREFIX + key;
+    
+    const [value, setValue] = useState(()=>{
+        const jsonValue = localStorage.getItem(prefixedkey)
+        if(jsonValue!== null) return JSON.parse(jsonValue);
+
+        if(typeof initialValue === 'function'){
+            return initialValue()
+        }else{
+            return initialValue;
+        }
+    })
+
+    useEffect(() => {
+        localStorage.setItem(prefixedkey, JSON.stringify(value))
+    }, [prefixedkey,value])
+
+    return [value,setValue];
+}
